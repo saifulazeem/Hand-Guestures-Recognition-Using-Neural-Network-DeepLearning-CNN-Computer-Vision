5ef12e7a1d6a9ce17950d89746de1b974bb41da4
MUHAMMAD SAIF UL AZEEM ABBASI <57259157+saifulazeem@users.noreply.github.com>
2021-01-23 14:21:31 +0500

    Collecting Data for Training

---
+++ data_collection.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import numpy as np
+import os
+
+# Create the directory structure
+if not os.path.exists("data"):
+    os.makedirs("data")
+    os.makedirs("data/train")
+    os.makedirs("data/test")
+    os.makedirs("data/train/A")
+    os.makedirs("data/train/B")
+    os.makedirs("data/train/C")
+    os.makedirs("data/train/D")
+    os.makedirs("data/train/E")
+    os.makedirs("data/train/F")
+    os.makedirs("data/train/G")
+    os.makedirs("data/test/A")
+    os.makedirs("data/test/B")
+    os.makedirs("data/test/C")
+    os.makedirs("data/test/D")
+    os.makedirs("data/test/E")
+    os.makedirs("data/test/F")
+    os.makedirs("data/test/G")
+    
+
+# Train or test 
+mode = 'train'
+directory = 'data/'+mode+'/'
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    # Simulating mirror image
+    frame = cv2.flip(frame, 1)
+    
+    # Getting count of existing images
+    count = {'A': len(os.listdir(directory+"/A")),
+             'B': len(os.listdir(directory+"/B")),
+             'C': len(os.listdir(directory+"/C")),
+             'D': len(os.listdir(directory+"/D")),
+             'E': len(os.listdir(directory+"/E")),
+             'F': len(os.listdir(directory+"/F")),
+             'G': len(os.listdir(directory+"/G"))}
+    
+    # Printing the count in each set to the screen
+    cv2.putText(frame, "MODE : "+mode, (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "IMAGE COUNT", (10, 100), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "A : "+str(count['A']), (10, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "B : "+str(count['B']), (10, 140), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "C : "+str(count['C']), (10, 160), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "D : "+str(count['D']), (10, 180), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "E : "+str(count['E']), (10, 200), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "F : "+str(count['F']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "G : "+str(count['G']), (10, 240), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    
+    # Coordinates of the ROI
+    x1 = int(0.5*frame.shape[1])
+    y1 = 65
+    x2 = frame.shape[1]-10
+    y2 = int(0.5*frame.shape[1])
+    # Drawing the ROI
+    # The increment/decrement by 1 is to compensate for the bounding box
+    cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (255,0,0) ,1)
+    # Extracting the ROI
+    roi = frame[y1:y2, x1:x2]
+    roi = cv2.resize(roi, (64, 64)) 
+ 
+    cv2.imshow("Frame", frame)
+    
+    #_, mask = cv2.threshold(mask, 200, 255, cv2.THRESH_BINARY)
+    #kernel = np.ones((1, 1), np.uint8)
+    #img = cv2.dilate(mask, kernel, iterations=1)
+    #img = cv2.erode(mask, kernel, iterations=1)
+    # do the processing after capturing the image!
+    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    _, roi = cv2.threshold(roi, 120, 255, cv2.THRESH_BINARY)
+    cv2.imshow("ROI", roi)
+    
+    interrupt = cv2.waitKey(10)
+    if interrupt & 0xFF == 27: # esc key
+        break
+    if interrupt & 0xFF == ord('A'):
+        cv2.imwrite(directory+'A/'+str(count['A'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('B'):
+        cv2.imwrite(directory+'B/'+str(count['B'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('C'):
+        cv2.imwrite(directory+'C/'+str(count['C'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('D'):
+        cv2.imwrite(directory+'D/'+str(count['D'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('E'):
+        cv2.imwrite(directory+'E/'+str(count['E'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('F'):
+        cv2.imwrite(directory+'F/'+str(count['F'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('G'):
+        cv2.imwrite(directory+'G/'+str(count['G'])+'.jpg', roi)
+    
+cap.release()
+cv2.destroyAllWindows()
+"""
+d = "old-data/test/A"
+newd = "data/test/A"
+for walk in os.walk(d):
+    for file in walk[2]:
+        roi = cv2.imread(d+"/"+file)
+        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        _, mask = cv2.threshold(roi, 120, 255, cv2.THRESH_BINARY)
+        cv2.imwrite(newd+"/"+file, mask)     
+"""
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
